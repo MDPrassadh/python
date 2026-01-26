5e128c61368d7554b99fff03eaf336a65dfebae8
MDPrassadh <mdprassadh@gmail.com>
2026-01-26 13:08:08 +0530

    paython basics .py and comments

---
+++ 02 comments.py
@@ -0,0 +1,8 @@
+# Single line comments are created using the hash symbol (#).
+# Everything after the # on that line is ignored by Python.
+print("Welcome to Python programming language")  # This prints a welcome message 
+
+''' This is a multi-line comment
+this type of comemnts we have to use in coding documantation '''
+
+print("This is an example of multi-line comment usage.")
