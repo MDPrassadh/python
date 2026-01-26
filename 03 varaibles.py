e52cca964823515a4a791d3212cb16098ccdfcf4
MDPrassadh <mdprassadh@gmail.com>
2026-01-26 22:26:45 +0530

    03 varaibles.py

--- 03 varaibles.py
+++ 03 varaibles.py
@@ -1,62 +1,62 @@
-# a=10
-# print(a)
+a=10
+print(a)
 
-# a,b,c=5
-# print(a)
-# print(b)    
-# print(c)
-# print(a,b,c) 
-# everything error the above case
+a,b,c=5
+print(a)
+print(b)    
+print(c)
+print(a,b,c) 
+everything error the above case
 
-#:python is case sensitive language
-# a=10
-# B=20
-# b=30
-# A=40
-# print(a)
-# print(A)
-# print(B)    
-# print(b)    
-# 10
-# 40
-# 20
-# 30
+:python is case sensitive language
+a=10
+B=20
+b=30
+A=40
+print(a)
+print(A)
+print(B)    
+print(b)    
+10
+40
+20
+30
 
-# c=(1,2,3)
-# print(c)
+c=(1,2,3)
+print(c)
 
-# output:(1, 2, 3) is called tuple
+output:(1, 2, 3) is called tuple
 
-# a=b=c=100
-# print(a)
-# print(b)
-# print(c)
-# print(a+b+c)
-# print(a,b,c)  # 100 100 100
+a=b=c=100
+print(a)
+print(b)
+print(c)
+print(a+b+c)
+print(a,b,c)  # 100 100 100
 
-# output:
-# 100
-# 100
-# 100
-# 300
+output:
+100
+100
+100
+300
 
-# prassadh=999
-# print(prassadh)
+prassadh=999
+print(prassadh)
 
-# output:999
+output:999
 
-# 1mdp=10
-# print(1mdp) # invalid syntax error
+1mdp=10
+print(1mdp) # invalid syntax error
 
-# $mdp=10
-# print($mdp)  # invalid syntax error
+$mdp=10
+print($mdp)  # invalid syntax error
 
-# _mdp=55
-# print(_mdp)  #55 its valid through (underscore  is allowed)
+_mdp=55
+print(_mdp)  #55 its valid through (underscore  is allowed)
 
-# arun_kumar123=1000  
-# print(arun_kumar123)  #1000 its valid through (alphabates , numbers and underscore is allowed)  
+arun_kumar123=1000  
+print(arun_kumar123)  #1000 its valid through (alphabates , numbers and underscore is allowed)  
 
-# mdp=15
-# print(id(mdp))  # 140719680812600 it used for memory location
+mdp=15
+print(id(mdp))  # 140719680812600 it used for memory location
 
