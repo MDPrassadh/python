5a411316a25448a328f018e560e9fe4db9d52b4e
MDPrassadh <mdprassadh@gmail.com>
2026-01-29 14:55:07 +0530

    set usage and its kinds

---
+++ 10 set.py
@@ -0,0 +1,34 @@
+SET-Introduction
+ 
+s={}
+print(type(s))
+
+ACTICE/10 set.py"
+<class 'dict'> By default it shows disctionary dependes upon set configured values
+
+s={2,3,5,4,6}
+print(type(s))
+
+ACTICE/10 set.py"
+<class 'set'> you set elements in curly braces it will twll "set"  
+
+No Duplication allow---
+a={2,3,4,2,6,6,6,7}
+print(a)
+
+-output :
+n.exe "d:/PYTHON-PRACTICE/10 set.py"
+{2, 3, 4, 6, 7}  
+
+SET METHODS-------------
+
+
+
+
+
+
+
+
+
+
+
