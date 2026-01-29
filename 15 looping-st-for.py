388f7be4d421b1054d4217588ecfc2436f336c47
MDPrassadh <mdprassadh@gmail.com>
2026-01-29 16:37:28 +0530

    looping-statements--for i in range:

---
+++ 15 looping-st-for.py
@@ -0,0 +1,55 @@
+Looping Statements:----
+
+for i in range   [-----this is the syntax for loop]
+    print(i)
+
+i want loop of (0,10) print so here is syntax for that
+
+for i in range(10):  #(0,10)
+    print(i)
+    
+#--output--
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+Now i need a slice of upto 10 numbers slicing like this
+for i in range(0,10,2):
+    print(i)
+    
+#output--
+0
+2
+4
+6
+8
+
+Now i need a slice of upto 10 numbers slicing like this
+for i in range(1,10,2):
+    print(i)
+    
+#output--
+1
+3
+5
+7
+9
+
+r=[2,4,6,7,9]  # here im using list
+for i in r:
+    print(i)
+
+#output--
+2
+4
+6
+7
+9
+
