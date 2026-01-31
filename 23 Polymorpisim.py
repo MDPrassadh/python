b7ce0e64805475c380cff61d11325b7e94f207dc
MDPrassadh <mdprassadh@gmail.com>
2026-01-31 16:18:00 +0530

    Polymorpism data concatination list string integer tuple

---
+++ 23 Polymorpisim.py
@@ -0,0 +1,20 @@
+'''
+One person but exercise in different roles is called polymorpism
+like male person acting like a son brother father grandfather husband friend smae person 
+but gracing too many character depends on circumstances
+
+
+
+def add(a,b):
+    return(a+b)
+add(2,5)
+add('p','q')
+add([5,7],[3,6])
+'''
+def add(a,b):
+    print(a+b)
+    
+add(2,5)          # 7
+add('p','q')      # pq
+add([5,7],[3,6])  # [5, 7, 3, 6]
+
