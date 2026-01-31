b54b9e2a0a896401b8c94f85a98d5cfeeab38572
MDPrassadh <mdprassadh@gmail.com>
2026-01-31 16:21:33 +0530

    Data-Abstraction

---
+++ 24 Data-Abstraction.py
@@ -0,0 +1,5 @@
+'''
+Docstring for 24 Data-Abstraction
+Hiding the information like
+The Process of Hiding the real implementation of an application from the user and emphasizing only on usage of it.
+'''
